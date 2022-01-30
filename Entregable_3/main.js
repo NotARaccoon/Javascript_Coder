143c10fa00bf6d345485c58d5e24157d2510df96
Alvaro <alvaroccjs@gmail.com>
2022-01-30 14:38:44 -0500

    Proyecto final
    inicios...

--- Entregable_3/main.js
+++ Entregable_3/main.js
@@ -1,12 +1,14 @@
 let nombreJugador = prompt("Ingresa tu nombre de entrenador");
 
 class Party {
-    constructor(pokemonNombre,pokemonTipo,nivel){
+    constructor(pokemonNombre,pokemonTipo,nivel,hp){
         this.pokemonNombre = pokemonNombre;
         this.pokemonTipo = pokemonTipo;
         this.nivel = parseInt(nivel);
+        this.hp = parseInt(hp);
     }
 }
+
 
 let party = [];
 let maxParty = 6
@@ -16,14 +18,66 @@ do{
     let pokemonNombre = prompt("Ingresa el nombre de tu pokemon numero "+ pokeNum);
     let pokemonTipo = prompt("Ingresa su tipo");
     let nivel = 1;
-    const pokemon = new Party(pokemonNombre,pokemonTipo,nivel);
+    let hp = Math.floor(Math.random()* (24 - 19)+19);
+    const pokemon = new Party(pokemonNombre,pokemonTipo,nivel,hp);
     party.push(pokemon);
     pokeNum++;
 } while (party.length != maxParty);
 
 console.log(party);
 
+/*function deleteP(){
+    let battlePoke = prompt("Selecciona a quien quieres eliminar");
+    let id;
+    battlePoke = party.pokemonNombre;
+    for(let i = 0; i>party.length;i++){
+        if(party.length.id == battlePoke.length.id){
+            delete party[id]
+        }
+    }
+    delete party[pokemonNombre];
+    console.log(party);
+}*/
 
+function increaseHP(pokemonBattleling){
+    while(nivel <= nivelActual){
+        hp = party.hp
+        hp += Math.floor(Math.random()*(3 - 1)+1);
+        nivel++;
+    }
+} 
+
+function battle(){
+
+    let battlePoke = prompt("Selecciona al pokemon que quieras usar");
+
+    let correct = true;
+    while(correct == false){
+        if(battlePoke == party.pokemonNombre){
+
+            let enemy;
+            let nivelActual
+
+            alert("Que la batalla comienze");
+            alert("Viendo resultados...");
+
+            battleWIN = Math.floor(Math.random()* (1 - 0)+0)
+
+            if (battleWIN == 1){
+
+                increaseHP(battlePoke);
+                correct = true;
+            } else{
+                alert("Perdiste la batalla");
+            }
+        }
+    }
+}
+
+
+
+
+  
 /* 
 while(party.length < 6){
     function createPokemon(){
