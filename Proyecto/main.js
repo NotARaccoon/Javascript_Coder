20e6515a619c7f061bd6cb8b025991c5f42edf82
Alvaro <alvaroccjs@gmail.com>
2022-02-07 18:49:51 -0500

    Html
    Implementacion de html con java, y css

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -1,28 +1,46 @@
 let nombreJugador = prompt("Ingresa tu nombre de entrenador");
 
+const partySetup = document.querySelector(".party-setup");
+
 class Party {
-    constructor(pokemonNombre,pokemonTipo,nivel,hp){
+    constructor(pokemonNombre,pokemonTipo,nivel,hp,pokemonimg){
         this.pokemonNombre = pokemonNombre;
         this.pokemonTipo = pokemonTipo;
         this.nivel = parseInt(nivel);
         this.hp = parseInt(hp);
+        this.pokemonimg =  pokemonimg;
     }
 }
 
 
 let party = [];
 let element = ["AGUA","FUEGO","HIELO"]
-let maxParty = 6
+let maxParty = 6;
 let pokeNum = 1;
 
 do{
     let pokemonNombre = prompt("Ingresa el nombre de tu pokemon numero "+ pokeNum);
     let pokemonTipo = prompt("Ingresa su elemento");
     let pokemonTipoUP = pokemonTipo.toUpperCase();
+    let pokemonimg;
     if(element.includes(pokemonTipoUP)){
         let nivel = 1;
         let hp = Math.floor(Math.random()* (24 - 19)+19);
-        const pokemon = new Party(pokemonNombre,pokemonTipo,nivel,hp);
+        switch (pokemonTipoUP){
+            case "HIELO":
+                pokemonimg = '../Proyecto/img/favpng_symbol-ice-emblem-logo.png';
+                break;
+            
+            case "FUEGO":
+                pokemonimg= '../Proyecto/img/fire-element-png-Transparent-Images.png';
+                break;
+
+            case "AGUA":
+                pokemonimg= '../Proyecto/img/217-2172033_water-png-water-element-transparent.png';
+                break;
+        }
+        const pokemon = new Party(pokemonNombre,pokemonTipo,nivel,hp,pokemonimg);
+        
         party.push(pokemon);
         pokeNum++;
     } else{
@@ -32,7 +50,44 @@ do{
     
 } while (party.length != maxParty);
 
-console.log(party);
+console.log(party.pokemonNombre);
+
+displayParty();
+
+function displayParty(){
+
+    party.forEach(function (dparty){
+
+        console.log(dparty);
+        
+        const divParty = document.createElement('div');
+        divParty.classList.add('card');
+
+        const imgParty = document.createElement('img');
+        imgParty.classList.add('party-img')
+        imgParty.src = dparty.pokemonimg;
+
+        const nombrePoke = document.createElement('h2');
+        nombrePoke.classList.add('nombre-pokemon')
+        nombrePoke.textContent = dparty.pokemonNombre;
+
+        const nivelPoke = document.createElement('h2');
+        nivelPoke.classList.add('nivel-pokemon')
+        nivelPoke.textContent = dparty.pokemonLevel;
+
+        const elementoPoke = document.createElement('h3');
+        elementoPoke.classList.add('elemnto-poke');
+        elementoPoke.textContent = dparty.pokemonTipo
+
+        divParty.appendChild(imgParty);
+        divParty.appendChild(nombrePoke);
+        divParty.appendChild(nivelPoke);
+        divParty.appendChild(elementoPoke);
+
+        partySetup.appendChild(divParty);
+
+    })
+}
 
 function battle(){
 
@@ -95,9 +150,9 @@ do {
 console.log(decision);
 
 function increaseHP(pokemonBattleling){
-    pokemonBattleling = party.nivel;
+    pokemonBattleling = party.find((pk)=> pk.nivel == pokemonBattleling);
     while(pokemonBattleling <= nivelActual){
-        hp = party.hp
+        hp = party.find((pk)=> pk.hp == hp)
         hp += Math.floor(Math.random()*(3 - 1)+1);
         nivel++;
     }
